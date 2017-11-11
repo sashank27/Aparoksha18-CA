b16717677fd00592ec0d6ca71e3ba052e9564045
Akshat Jain <lit2016016@iiita.ac.in>
2017-11-11 16:46:30 +0530

    upload from camera added

--- app/src/main/java/org/aparoksha/app18/ca/MainActivity.kt
+++ app/src/main/java/org/aparoksha/app18/ca/MainActivity.kt
@@ -1,24 +1,29 @@
 package org.aparoksha.app18.ca
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import com.firebase.ui.auth.AuthUI
-import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 import java.util.*
+import android.util.Log
+import android.graphics.Bitmap
+import android.R.attr.data
+import android.content.Context
+import android.net.Uri
+import android.support.v4.app.NotificationCompat.getExtras
+import android.provider.MediaStore.Images
+import org.jetbrains.anko.UI
+import java.io.ByteArrayOutputStream
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +34,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mFirebaseStorage: FirebaseStorage
     private val RC_SIGN_IN = 1
     private val RC_PHOTO_PICKER = 2
+    private val CAMERA_REQUEST = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,13 +43,18 @@ class MainActivity : AppCompatActivity() {
         mFirebaseAuth = FirebaseAuth.getInstance()
         mFirebaseStorage = FirebaseStorage.getInstance()
 
-        mStorageReference = mFirebaseStorage.getReference()
+        mStorageReference = mFirebaseStorage.getReference().child("cam")
 
         upload.setOnClickListener(View.OnClickListener { view ->
                 val intent = Intent(Intent.ACTION_GET_CONTENT)
                 intent.type = "image/jpeg"
                 intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true)
-                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER)
+                startActivityForResult(Intent.createChooser(intent, "Select Photo to be uploaded"), RC_PHOTO_PICKER)
+        })
+
+        camera.setOnClickListener(View.OnClickListener { view ->
+            val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, CAMERA_REQUEST)
         })
 
         mAuthStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
@@ -82,6 +93,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         super.onActivityResult(requestCode, resultCode, data)
+        Log.d("aaya","aaya")
         if (requestCode == RC_SIGN_IN) {
             if(resultCode == Activity.RESULT_OK) {
                 toast("Signed In")
@@ -92,18 +104,37 @@ class MainActivity : AppCompatActivity() {
         } else if(requestCode == RC_PHOTO_PICKER && resultCode == Activity.RESULT_OK) {
             if(mFirebaseAuth.currentUser != null) {
                 val SelectedImageUri = data.getData()
-                val userStorage = mStorageReference.child(mFirebaseAuth.currentUser.toString())
+                toast(SelectedImageUri.toString())
+                val userStorage = mStorageReference.child(mFirebaseAuth.currentUser!!.email.toString())
                 val Reference  = userStorage.child(mFirebaseAuth.currentUser!!.uid)
-                val photoRef = Reference.child(mFirebaseAuth.currentUser!!.email.toString())
+                val photoRef = Reference.child(System.currentTimeMillis().toString())
                 photoRef.putFile(SelectedImageUri).addOnSuccessListener(this) { taskSnapshot ->
-                    toast("Successfully Uploaded")
+                       toast("Successfully Uploaded")
                 }
+
             } else {
                 toast("Authentication Error")
                 userName = ""
                 AuthUI.getInstance().signOut(this)
             }
+        } else if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            val extras = data.extras
+            val imageBitmap = extras.get("data") as Bitmap
+            val SelectedImageUri = getImageUri(applicationContext, imageBitmap)
+            val userStorage = mStorageReference.child(mFirebaseAuth.currentUser!!.email.toString())
+            val Reference  = userStorage.child(mFirebaseAuth.currentUser!!.uid)
+            val photoRef = Reference.child(System.currentTimeMillis().toString())
+            photoRef.putFile(SelectedImageUri).addOnSuccessListener(this) { taskSnapshot ->
+                toast("Successfully Uploaded")
+            }
         }
+    }
+
+    fun getImageUri(inContext: Context, inImage: Bitmap): Uri {
+        val bytes = ByteArrayOutputStream()
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null)
+        return Uri.parse(path)
     }
 
     override fun onPause() {
