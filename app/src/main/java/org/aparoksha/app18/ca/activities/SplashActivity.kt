846d09201f408d0b84187a557c67b2c4fee940bf
betterclever <paliwal.pranjal83@gmail.com>
2017-12-21 21:44:58 +0530

    Improved Splash Animations

--- app/src/main/java/org/aparoksha/app18/ca/activities/SplashActivity.kt
+++ app/src/main/java/org/aparoksha/app18/ca/activities/SplashActivity.kt
@@ -3,12 +3,13 @@ package org.aparoksha.app18.ca.activities
 import android.os.Bundle
 import android.os.Handler
 import android.support.constraint.ConstraintSet
+import android.support.transition.ChangeBounds
+import android.support.transition.TransitionManager
 import android.support.v7.app.AppCompatActivity
-import android.transition.TransitionManager
+import android.view.animation.AnticipateOvershootInterpolator
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_splash.*
 import org.aparoksha.app18.ca.R
@@ -17,7 +18,7 @@ import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
 
-    private val SPLASH_DISPLAY_LENGTH: Long = 2500
+    private val SPLASH_DISPLAY_LENGTH: Long = 2400
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,21 +26,16 @@ class SplashActivity : AppCompatActivity() {
 
         Handler().postDelayed({
             val constraintSet = ConstraintSet()
-
-            constraintSet.clone(this@SplashActivity, R.layout.additional_splash)
-
-            TransitionManager.beginDelayedTransition(splash)
-            constraintSet.applyTo(splash)
-        }, 400)
-
-        Handler().postDelayed({
-            val constraintSet = ConstraintSet()
-
             constraintSet.clone(this@SplashActivity, R.layout.secondary_splash)
 
-            TransitionManager.beginDelayedTransition(splash)
+            val transition = ChangeBounds()
+            transition.interpolator = AnticipateOvershootInterpolator(1.0f)
+            transition.duration = 1200
+
+            TransitionManager.beginDelayedTransition(splash, transition)
             constraintSet.applyTo(splash)
-        }, 600)
+
+        }, 100)
 
         Handler().postDelayed({
             val mFirebaseAuth = FirebaseAuth.getInstance()
@@ -48,17 +44,17 @@ class SplashActivity : AppCompatActivity() {
                 val myRef = fetchDBCurrentUser()!!
                 myRef.addValueEventListener(object : ValueEventListener {
                     override fun onDataChange(dataSnapshot: DataSnapshot) {
-                        if(dataSnapshot.exists()){
+                        if (dataSnapshot.exists()) {
                             startActivity<MainActivity>()
                             myRef.removeEventListener(this)
                             finish()
-                        }
-                        else{
+                        } else {
                             startActivity<WelcomeActivity>()
                             myRef.removeEventListener(this)
                             finish()
                         }
                     }
+
                     override fun onCancelled(error: DatabaseError?) {
                         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                     }
