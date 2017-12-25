c2d16af428eba753d9e0272ab53e5cd93a984a8c
sashank27 <sashankmishra27@gmail.com>
2017-12-25 16:12:55 +0530

    CrashAnalytics dependencies added

---
+++ app/src/main/java/org/aparoksha/app18/ca/AparokshaGlideModule.kt
@@ -0,0 +1,25 @@
+package org.aparoksha.app18.ca
+
+import android.content.Context
+
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+import com.firebase.ui.storage.images.FirebaseImageLoader
+import com.google.firebase.storage.StorageReference
+
+import java.io.InputStream
+
+/**
+ * Created by betterclever on 18/12/17.
+ */
+@GlideModule
+class AparokshaGlideModule : AppGlideModule() {
+
+    override fun registerComponents(context: Context?, glide: Glide?, registry: Registry?) {
+        // Register FirebaseImageLoader to handle StorageReference
+        registry!!.append(StorageReference::class.java, InputStream::class.java,
+                FirebaseImageLoader.Factory())
+    }
+}
