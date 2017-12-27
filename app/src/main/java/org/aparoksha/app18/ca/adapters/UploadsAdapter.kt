8e482bab4b6326cae0be2bac5acb12f60350ad64
Akshat Jain <lit2016016@iiita.ac.in>
2017-12-27 23:03:36 +0530

    progress bar added

--- app/src/main/java/org/aparoksha/app18/ca/adapters/UploadsAdapter.kt
+++ app/src/main/java/org/aparoksha/app18/ca/adapters/UploadsAdapter.kt
@@ -1,12 +1,17 @@
 package org.aparoksha.app18.ca.adapters
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.storage.StorageReference
@@ -46,6 +51,20 @@ class UploadsAdapter(options: FirebaseRecyclerOptions<Image>,
             val imageRef = storageRef.child(image.path)
             GlideApp.with(context)
                     .load(imageRef)
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                            mView.progress_bar.visibility = View.GONE
+                            mView.error_loading.visibility = View.VISIBLE
+                            return false
+                        }
+
+                        override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                            mView.progress_bar.visibility = View.GONE
+                            mView.error_loading.visibility = View.GONE
+                            return false
+                        }
+
+                    })
                     .into(mView.image)
 
             val colorAccent = ContextCompat.getColor(context, R.color.colorAccent)
