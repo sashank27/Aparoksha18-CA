08ffe6251b6e7de8805746e5a6db6f9058b09dd6
Akshat Jain <lit2016016@iiita.ac.in>
2018-03-03 16:48:50 +0530

    Update: Version 2.4

--- app/src/main/java/org/aparoksha/app18/ca/AparokshaCA.kt
+++ app/src/main/java/org/aparoksha/app18/ca/AparokshaCA.kt
@@ -2,6 +2,7 @@ package org.aparoksha.app18.ca
 
 import android.app.Application
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.messaging.FirebaseMessaging
 
 /**
  * Created by betterclever on 18/12/17.
@@ -12,5 +13,6 @@ class AparokshaCA : Application() {
     override fun onCreate() {
         super.onCreate()
         FirebaseDatabase.getInstance().setPersistenceEnabled(true)
+        FirebaseMessaging.getInstance().subscribeToTopic("all")
     }
 }
