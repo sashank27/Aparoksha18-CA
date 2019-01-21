2080cd5f1c6092a188afb15cbd4625efb46f6131
sashank27 <sashankmishra27@gmail.com>
2019-01-21 19:22:45 +0530

    Remote Config added

--- app/src/main/java/org/aparoksha/app18/ca/AparokshaCA.kt
+++ app/src/main/java/org/aparoksha/app18/ca/AparokshaCA.kt
@@ -1,8 +1,12 @@
 package org.aparoksha.app18.ca
 
 import android.app.Application
+import android.util.Log
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import org.aparoksha.app18.ca.activities.SplashActivity
+import java.util.HashMap
 
 /**
  * Created by betterclever on 18/12/17.
@@ -14,5 +18,22 @@ class AparokshaCA : Application() {
         super.onCreate()
         FirebaseDatabase.getInstance().setPersistenceEnabled(true)
         FirebaseMessaging.getInstance().subscribeToTopic("all")
+
+        val firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+
+        // set in-app defaults
+        val remoteConfigDefaults: HashMap<String, Any> = HashMap()
+        remoteConfigDefaults[SplashActivity.KEY_UPDATE_REQUIRED] = false
+        remoteConfigDefaults[SplashActivity.KEY_CURRENT_VERSION] = "1.0.0"
+        remoteConfigDefaults[SplashActivity.KEY_UPDATE_URL] = "https://play.google.com/store/apps/details?id=org.aparoksha.app18.ca"
+
+        firebaseRemoteConfig.setDefaults(remoteConfigDefaults)
+        firebaseRemoteConfig.fetch(60)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Log.d("Remote Config", "remote config is fetched.")
+                        firebaseRemoteConfig.activateFetched()
+                    }
+                }
     }
 }
--- app/src/main/java/org/aparoksha/app18/ca/activities/SplashActivity.kt
+++ app/src/main/java/org/aparoksha/app18/ca/activities/SplashActivity.kt
@@ -1,5 +1,6 @@
 package org.aparoksha.app18.ca.activities
 
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.support.constraint.ConstraintSet
@@ -15,10 +16,66 @@ import kotlinx.android.synthetic.main.activity_splash.*
 import org.aparoksha.app18.ca.R
 import org.aparoksha.app18.ca.utils.fetchDBCurrentUser
 import org.jetbrains.anko.startActivity
+import android.support.v7.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.text.TextUtils
+import android.util.Log
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+
 
 class SplashActivity : AppCompatActivity() {
 
-    private val SPLASH_DISPLAY_LENGTH: Long = 2400
+    companion object {
+        private val SPLASH_DISPLAY_LENGTH: Long = 2400
+        private val TAG = SplashActivity::class.java.simpleName
+
+        const val KEY_UPDATE_REQUIRED = "force_update_required"
+        const val KEY_CURRENT_VERSION = "force_update_current_version"
+        const val KEY_UPDATE_URL = "force_update_store_url"
+    }
+
+    private fun redirectStore(updateUrl: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(updateUrl))
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    private fun check() : Boolean {
+        val remoteConfig = FirebaseRemoteConfig.getInstance()
+
+        return if (remoteConfig.getBoolean(KEY_UPDATE_REQUIRED)) {
+            val currentVersion = remoteConfig.getString(KEY_CURRENT_VERSION)
+            val appVersion = getAppVersion(this)
+            val updateUrl = remoteConfig.getString(KEY_UPDATE_URL)
+
+            return if (!TextUtils.equals(currentVersion, appVersion)) {
+                val dialog = AlertDialog.Builder(this)
+                        .setTitle("New version available")
+                        .setMessage("Please update app to new version to continue.")
+                        .setPositiveButton("Update") { _, _ -> redirectStore(updateUrl) }
+                        .setNegativeButton("No, thanks") { _, _ -> finish() }
+                        .create()
+                dialog.show()
+                true
+            } else false
+        } else false
+    }
+
+    private fun getAppVersion(context: Context): String {
+        var result = ""
+        try {
+            result = context.packageManager
+                    .getPackageInfo(context.packageName, 0)
+                    .versionName
+            result = result.replace("[a-zA-Z]|-".toRegex(), "")
+        } catch (e: PackageManager.NameNotFoundException) {
+            Log.e(TAG, e.message)
+        }
+        return result
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,30 +95,32 @@ class SplashActivity : AppCompatActivity() {
         }, 100)
 
         Handler().postDelayed({
-            val mFirebaseAuth = FirebaseAuth.getInstance()
+            if(!check()) {
+                val mFirebaseAuth = FirebaseAuth.getInstance()
 
-            if (mFirebaseAuth.currentUser != null) {
-                val myRef = fetchDBCurrentUser()!!
-                myRef.addValueEventListener(object : ValueEventListener {
-                    override fun onDataChange(dataSnapshot: DataSnapshot) {
-                        if (dataSnapshot.exists()) {
-                            startActivity<MainActivity>()
-                            myRef.removeEventListener(this)
-                            finish()
-                        } else {
-                            startActivity<WelcomeActivity>()
-                            myRef.removeEventListener(this)
-                            finish()
+                if (mFirebaseAuth.currentUser != null) {
+                    val myRef = fetchDBCurrentUser()!!
+                    myRef.addValueEventListener(object : ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                startActivity<MainActivity>()
+                                myRef.removeEventListener(this)
+                                finish()
+                            } else {
+                                startActivity<WelcomeActivity>()
+                                myRef.removeEventListener(this)
+                                finish()
+                            }
                         }
-                    }
 
-                    override fun onCancelled(error: DatabaseError?) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    }
-                })
-            } else {
-                startActivity<WelcomeActivity>()
-                finish()
+                        override fun onCancelled(error: DatabaseError?) {
+                            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                        }
+                    })
+                } else {
+                    startActivity<WelcomeActivity>()
+                    finish()
+                }
             }
         }, SPLASH_DISPLAY_LENGTH)
 
